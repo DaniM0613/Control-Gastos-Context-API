7a34b0a40a5fc375b910593c8371236c7dbac5f9
DaniM0613 <wwh.rj.92@gmail.com>
2024-04-23 22:25:15 -0600

    l

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,4 +1,4 @@
-import { useState, type ChangeEvent } from 'react';
+import { useState, useEffect, type ChangeEvent } from 'react';
 import type { DraftExpense, Value } from '../types';
 import { categories } from "../data/categories";
 import DatePicker from 'react-date-picker';
@@ -18,7 +18,14 @@ export default function ExpenseForm() {
    })
 
    const [error, setError ] = useState('')
-   const {dispatch} = useBudget()
+   const {dispatch, state} = useBudget()
+
+   useEffect(() => {
+       if(state.editingId) {
+          const editingExpense = state.expenses.filter( currentExpense => currentExpense.id === state.editingId) [0]
+          setExpense(editingExpense)
+       }
+   }, [state.editingId])
 
    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
     const {name, value} = e.target
@@ -45,9 +52,13 @@ export default function ExpenseForm() {
         return
       }
 
-      // Agregar un nuevo gasto
-      dispatch({type: 'add-expense', payload: { expense }})
-
+      // Agregar o actualizar el gasto
+      if(state.editingId) {
+        dispatch({type: 'update-expense', payload: {expense: {id: state.editingId, ...expense }}})
+      } else {
+        dispatch({type: 'add-expense', payload: { expense }})
+      }
+      
       // Reiniciar el State
         setExpense({
           amount: 0,
